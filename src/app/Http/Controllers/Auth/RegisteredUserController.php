72d6cba44194db7e231e8369642c9985589ae125
tk-Hofman <tokitoki1729@gmail.com>
2024-12-23 07:52:23 +0900

    user登録後、ログイン画面に遷移

--- src/app/Http/Controllers/Auth/RegisteredUserController.php
+++ src/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -44,8 +44,6 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
-        Auth::login($user);
-
         return redirect(route('dashboard', absolute: false));
     }
 }
