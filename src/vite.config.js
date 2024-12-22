c465361c90c104cf5c6867cef3aac365f090e460
tk-Hofman <tokitoki1729@gmail.com>
2024-12-23 07:43:15 +0900

    laravelBreeze追加

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,15 +1,20 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/js/app.js', 'resources/css/app.css'],
             refresh: true,
         }),
+        vue(),
     ],
-    build: {
-        outDir: 'public/build', // 出力先ディレクトリ
-        manifest: true, // Vite manifest を生成
+    server: {
+        host: '0.0.0.0',  // Docker環境向け
+        port: 5173,
+        hmr: {
+            host: 'localhost', // ホットリロード用のホスト名
+        },
     },
 });
